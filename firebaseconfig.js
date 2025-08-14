f50fc6c7c22bfefb445cdd4db2e4af84e9381579
Sushil choudhary <84304083+sushilcodex01@users.noreply.github.com>
2025-08-14 23:24:18 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -5,14 +5,14 @@ import { getStorage, connectStorageEmulator } from 'https://www.gstatic.com/fire
 
 // Firebase configuration object - replace with your actual config
 const firebaseConfig = {
-  apiKey: "AIzaSyDA6duS8SJ5Wr26qNxzBoge01Leestj-9o",
-    authDomain: "animal-planet-73497.firebaseapp.com",
-    databaseURL: "https://animal-planet-73497-default-rtdb.firebaseio.com",
-    projectId: "animal-planet-73497",
-    storageBucket: "animal-planet-73497.appspot.com",
-    messagingSenderId: "15025745338",
-    appId: "1:15025745338:web:f2d2e2644d822afea00183",
-    measurementId: "G-KP9582LMC3"
+  apiKey: "demo",
+    authDomain: "demo",
+    databaseURL: "demo",
+    projectId: "demo",
+    storageBucket: "demo",
+    messagingSenderId: "demo",
+    appId: "demo",
+    measurementId: "demo"
 };
 
 // Initialize Firebase
